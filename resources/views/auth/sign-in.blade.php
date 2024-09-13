f834655e1a0f53974ccadce3f709809b1e1c7730
Alyyy07 <alielhamied1@gmail.com>
2024-09-13 22:57:29 +0700

    [FEAT] create new login page

---
+++ resources/views/auth/sign-in.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>E-RPL | Unija</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+		<link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico')}}" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+		<link href="{{ asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
+		<link href="{{ asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+	</head>
+	<body id="kt_body" class="app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center">
+		<script>
+            var defaultThemeMode = "light"; var themeMode; if ( document.documentElement ) { if ( document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if ( localStorage.getItem("data-bs-theme") !== null ) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }
+        </script>
+		<div class="d-flex flex-column flex-root" id="kt_app_root">
+			<!--begin::Page bg image-->
+			<style>body { background-image: url('assets/media/auth/bg10.jpeg'); } [data-bs-theme="dark"] body { background-image: url('assets/media/auth/bg10-dark.jpeg'); }</style>
+			<!--end::Page bg image-->
+			<!--begin::Authentication - Sign-in -->
+			<div class="d-flex flex-column flex-lg-row flex-column-fluid">
+				<!--begin::Aside-->
+				<div class="d-flex flex-lg-row-fluid">
+					<!--begin::Content-->
+					<div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
+						<!--begin::Image-->
+						<img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency.png" alt="" />
+						<img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency-dark.png" alt="" />
+						<!--end::Image-->
+						<!--begin::Title-->
+						<h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
+						<!--end::Title-->
+						<!--begin::Text-->
+						<div class="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
+						<a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve interviewed
+						<br />and provides some background information about
+						<a href="#" class="opacity-75-hover text-primary me-1">the interviewee</a>and their
+						<br />work following this is a transcript of the interview.</div>
+						<!--end::Text-->
+					</div>
+					<!--end::Content-->
+				</div>
+				<!--begin::Aside-->
+				<!--begin::Body-->
+				<div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
+					<!--begin::Wrapper-->
+					<div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
+						<!--begin::Content-->
+						<div class="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
+							<!--begin::Wrapper-->
+							<div class="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
+								<!--begin::Form-->
+								<form class="form w-100" method="POST" action="{{ route('login') }}">
+									@csrf
+									<div class="text-center mb-11">
+										<h1 class="text-dark fw-bolder mb-3">Selamat Datang</h1>
+										<div class="text-gray-500 fw-semibold fs-6">Silahkan Login untuk Mengakses E-Rpl</div>
+									</div>
+									<div class="fv-row mb-8">
+										<input type="text" placeholder="Email" name="email" value="{{ old('email') }}" autocomplete="off" class="form-control bg-transparent @error('email') is-invalid @enderror" />
+                                        @error('email')
+                                           <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+									</div>
+									<div class="fv-row mb-3">
+                                        <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent @error('password') is-invalid @enderror" />
+                                        @error('password')
+                                           <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+									</div>
+                                    <div class="block mt-4">
+                                        <label for="remember_me" class="inline-flex items-center">
+                                            <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                                            <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                                        </label>
+                                    </div>
+									<div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+										<div></div>
+									</div>
+									<!--end::Wrapper-->
+									<!--begin::Submit button-->
+									<div class="d-grid mb-10">
+										<button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+											<!--begin::Indicator label-->
+											<span class="indicator-label">Log In</span>
+											<!--end::Indicator label-->
+											<!--begin::Indicator progress-->
+											<span class="indicator-progress">Please wait...
+											<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+											<!--end::Indicator progress-->
+										</button>
+									</div>
+									<!--end::Submit button-->
+								</form>
+								<!--end::Form-->
+							</div>
+						</div>
+						<!--end::Content-->
+					</div>
+					<!--end::Wrapper-->
+				</div>
+				<!--end::Body-->
+			</div>
+			<!--end::Authentication - Sign-in-->
+		</div>
+		<!--end::Root-->
+		<script src="assets/plugins/global/plugins.bundle.js"></script>
+		<script src="assets/js/scripts.bundle.js"></script>
+	</body>
+	<!--end::Body-->
+</html>
