e315dd8f8d1c6e0a4f31ea2152a3a79976e9749d
Alyyy07 <alielhamied1@gmail.com>
2024-10-25 14:10:26 +0700

    [FIX] add permission checking for button

--- resources/views/admin/user-list/index.blade.php
+++ resources/views/admin/user-list/index.blade.php
@@ -93,20 +93,12 @@
                                         <span class="path1"></span>
                                         <span class="path2"></span>
                                     </i>Export</button>
-                                <button type="button" button-action="show" modal-id="#user-modal"
+                                    @can($globalModule['create'])
+                                    <button type="button" button-action="show" modal-id="#user-modal"
                                     button-url="{{ route('user-management.users.create') }}" class="btn btn-primary">
                                     <i class="ki-duotone ki-plus fs-2"></i>Add User</button>
+                                    @endcan
                             </div>
-                            {{-- <div user-toolbar="bulk-delete"
-                                class="d-flex justify-content-end align-items-center d-none"
-                                data-kt-user-table-toolbar="selected">
-                                <div class="fw-bold me-5">
-                                    <span class="me-2" data-user-selected></span>Selected
-                                </div>
-                                <button type="button" class="btn btn-danger"
-                                    button-url="{{ route('user-management.users.bulkDelete') }}"
-                                    button-action="bulk-delete">Delete Selected</button>
-                            </div> --}}
                             <div class="btn-group d-none" user-toolbar="selected-user">
                                 <button type="button" selected-button class="btn btn-danger"></button>
                                 <button type="button" dropdown-option
