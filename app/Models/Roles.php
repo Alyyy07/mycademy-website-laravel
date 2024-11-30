31c31b417b6a11a032c55517c6b71cbe1ed8813e
Alyyy07 <alielhamied1@gmail.com>
2024-11-30 14:58:46 +0700

    [FIX] make role accessible and dynamic

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -21,4 +21,9 @@ class Roles extends Model
         return $this->belongsToMany(Permission::class, 'role_has_permissions', 'role_id', 'permission_id');
     }
 
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'model_has_roles', 'role_id', 'model_id');
+    }
+
 }
--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -1,45 +1,37 @@
 @extends('layouts.partials.admin.app')
 @section('content')
 <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-5 g-xl-9">
+    @foreach ($roles as $role)
     <div class="col-md-4">
         <div class="card card-flush h-md-100">
             <div class="card-header">
-                <div class="card-title">
-                    <h2>Administrator</h2>
+                <div class="card-title text-capitalize">
+                    <h2>{{ $role['role'] }}</h2>
                 </div>
             </div>
             <div class="card-body pt-1">
-                <div class="fw-bold text-gray-600 mb-5">Total users with this role: 5</div>
+                <div class="fw-bold text-gray-600 mb-5">Total users with this role: {{ $role['users'] }}</div>
                 <div class="d-flex flex-column text-gray-600">
+                    @foreach ($role['permissions'] as $permission)
                     <div class="d-flex align-items-center py-2">
-                        <span class="bullet bg-primary me-3"></span>All Admin Controls
+                        <span class="bullet bg-primary me-3"></span>{{ $permission }}
                     </div>
+                    @if($loop->iteration >= 5)
                     <div class="d-flex align-items-center py-2">
-                        <span class="bullet bg-primary me-3"></span>View and Edit Financial Summaries
+                        <span class="bullet bg-primary me-3 italic"></span> and {{ $loop->remaining }} more...
                     </div>
-                    <div class="d-flex align-items-center py-2">
-                        <span class="bullet bg-primary me-3"></span>Enabled Bulk Reports
-                    </div>
-                    <div class="d-flex align-items-center py-2">
-                        <span class="bullet bg-primary me-3"></span>View and Edit Payouts
-                    </div>
-                    <div class="d-flex align-items-center py-2">
-                        <span class="bullet bg-primary me-3"></span>View and Edit Disputes
-                    </div>
-                    <div class='d-flex align-items-center py-2'>
-                        <span class='bullet bg-primary me-3'></span>
-                        <em>and 7 more...</em>
-                    </div>
+                    @break
+                    @endif
+                    @endforeach
                 </div>
             </div>
-            <div class="card-footer flex-wrap pt-0">
-                <a href="../../demo1/dist/apps/user-management/roles/view.html"
-                    class="btn btn-light btn-active-primary my-1 me-2">View Role</a>
+            <div class="card-footer flex-wrap pt-0 justify-content-center d-flex">
                 <button type="button" class="btn btn-light btn-active-light-primary my-1" data-bs-toggle="modal"
                     data-bs-target="#kt_modal_update_role">Edit Role</button>
             </div>
         </div>
     </div>
+    @endforeach
     <div class="ol-md-4">
         <div class="card h-md-100">
             <div class="card-body d-flex flex-center">
