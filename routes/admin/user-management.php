d7472bb3a8eb5966bc888a45797df0820371e926
Alyyy07 <alielhamied1@gmail.com>
2024-11-30 15:00:05 +0700

    [FIX] update accessible route

--- routes/admin/user-management.php
+++ routes/admin/user-management.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\admin\UserListController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'user-management', 'as' => 'user-management.'], function () {
-    Route::resource('/users', UserListController::class)->names('users');
+    Route::resource('/users', UserListController::class)->names('users')->except('show');
     Route::patch('/users/set-status/{user}', [UserListController::class, 'setStatus'])->name('users.setStatus');
     Route::post('/users/bulk-set-status/', [UserListController::class, 'bulkSetStatus'])->name('users.bulkSetStatus');
     Route::delete('/users',[UserListController::class,'bulkDelete'])->name('users.bulkDelete');
     
-    Route::resource('/roles', RoleController::class)->names('roles');
+    Route::resource('/roles', RoleController::class)->names('roles')->except('show');
 });
