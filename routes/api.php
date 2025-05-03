37063ea07a1fa4bb4197761ef3697d81a25de7a4
Alyyy07 <alielhamied1@gmail.com>
2025-05-03 17:12:45 +0700

    [FEAT] add route api for detail materi

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,10 @@ Route::prefix('auth')->middleware(['checkClientId'])->group(function () {
 
     Route::get('matakuliah',[AuthApiController::class, 'getMataKuliah']);
     Route::get('modul-pembelajaran',[AuthApiController::class, 'getMataKuliahById']);
+    Route::get('modul-pembelajaran/materi',[AuthApiController::class, 'getMateri']);
+    Route::post('modul-pembelajaran/materi-selesai',[AuthApiController::class, 'setMateriSelesai']);
+    Route::get('modul-pembelajaran/kuis',[AuthApiController::class, 'getKuis']);
+    Route::post('modul-pembelajaran/kuis-selesai',[AuthApiController::class, 'setKuisSelesai']);
 });
 
 Route::middleware(['checkClientId', 'throttle:60,1'])->group(function () {
