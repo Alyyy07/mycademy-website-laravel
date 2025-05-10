19e53d9fe8f574691e528da7347bc95dc1689838
Alyyy07 <alielhamied1@gmail.com>
2025-05-10 16:38:51 +0700

    [FEAT] add mapping matakuliah data

--- database/seeders/MappingMatakuliahSeeder.php
+++ database/seeders/MappingMatakuliahSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\MappingMatakuliah;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class MappingMatakuliahSeeder extends Seeder
@@ -12,10 +13,21 @@ class MappingMatakuliahSeeder extends Seeder
      */
     public function run(): void
     {
+        $dosenIot = User::where('name','like','Arda Gusema%')->first();
+        $dosenAndroid = User::where('name','like','Iddrus%')->first();
+        
+         MappingMatakuliah::create([
+            'tahun_ajaran_id' => '5',
+            'matakuliah_id' => '5',
+            'dosen_id' => $dosenIot->id,
+            'admin_verifier_id' => 'd7d74b70-bfb2-463d-b8aa-e3ee8ce95b71',
+            'semester' => '8',
+        ]);
+
         MappingMatakuliah::create([
             'tahun_ajaran_id' => '5',
-            'matakuliah_id' => '4',
-            'dosen_id' => 'b4bbc461-be6c-4464-b529-aca0e8ab3719',
+            'matakuliah_id' => '14',
+            'dosen_id' => $dosenAndroid->id,
             'admin_verifier_id' => 'd7d74b70-bfb2-463d-b8aa-e3ee8ce95b71',
             'semester' => '8',
         ]);
